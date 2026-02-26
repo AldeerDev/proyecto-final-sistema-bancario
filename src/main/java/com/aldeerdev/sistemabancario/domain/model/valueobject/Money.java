82faa4f1bf259d2c0d672605f445e8fcc2db83d7
AldeerDev <aldeerdev@gmail.com>
2026-02-26 20:15:29 -0300

    refactor: cambia el lanzamiento de error por InvalidMoneyException

--- src/main/java/com/aldeerdev/sistemabancario/domain/model/valueobject/Money.java
+++ src/main/java/com/aldeerdev/sistemabancario/domain/model/valueobject/Money.java
@@ -2,6 +2,7 @@ package com.aldeerdev.sistemabancario.domain.model.valueobject;
 
 import java.math.BigDecimal;
 
+import com.aldeerdev.sistemabancario.domain.exceptions.InvalidMoneyException;
 import com.aldeerdev.sistemabancario.domain.model.enums.TipoMoneda;
 
 public class Money {
@@ -10,15 +11,16 @@ public class Money {
 	private final TipoMoneda moneda;
 
 	public Money(BigDecimal cantidad, TipoMoneda moneda) {
+
 		if (cantidad == null) {
-			throw new RuntimeException("Cantidad no puede ser nula");
+			throw new InvalidMoneyException("Cantidad no puede ser nula");
 		}
-		
-		if(moneda == null) {
-			throw new RuntimeException("moneda no puede ser nula");
+
+		if (moneda == null) {
+			throw new InvalidMoneyException("moneda no puede ser nula");
 		}
 		this.cantidad = cantidad;
 		this.moneda = moneda;
 	}
-	
+
 }
