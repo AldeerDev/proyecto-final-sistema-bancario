db29975a7f6e93bb8a62e6a2cd14b265bc106314
AldeerDev <aldeerdev@gmail.com>
2026-03-02 20:15:27 -0300

    feat: agrega metodo de depositar y retirar con validaciones

--- src/main/java/com/aldeerdev/sistemabancario/domain/model/entity/CuentaBancaria.java
+++ src/main/java/com/aldeerdev/sistemabancario/domain/model/entity/CuentaBancaria.java
@@ -1,9 +1,52 @@
 package com.aldeerdev.sistemabancario.domain.model.entity;
 
+import java.util.Objects;
+
+import com.aldeerdev.sistemabancario.domain.model.valueobject.Money;
+
 public abstract class CuentaBancaria {
 
 	private String numeroCuenta;
 	private String titular;
-	private double saldo;
+	private Money saldo;
+
+	public abstract Money calcularInteres();
+
+	public void depositar(Money monto) {
+		// validar argumento nulo
+		Objects.requireNonNull(monto, "Monto no debe ser nulo");
+
+		this.saldo = saldo.sumar(monto);
+	}
+
+	public void retirar(Money monto) {
+		// validar agumento nulo
+		Objects.requireNonNull(monto, "Monto no debe ser nulo");
+
+		// validar que monto no sea mayor al saldo
+		if (saldo.getCantidad().compareTo(monto.getCantidad()) < 0)
+			throw new RuntimeException("Saldo Insuficiente");
+
+		// operar
+		this.saldo = saldo.restar(monto);
+	}
+
+	public void mostrarInformacion() {
+
+	}
+
+	// Getters
+
+	public String getNumeroCuenta() {
+		return numeroCuenta;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public Money getSaldo() {
+		return saldo;
+	}
 
 }
