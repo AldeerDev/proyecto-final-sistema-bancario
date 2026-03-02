1f292586454baaa5a5e0539885967e8ee400f464
AldeerDev <aldeerdev@gmail.com>
2026-03-02 20:21:46 -0300

    feat: agrega modelo Cliente

---
+++ src/main/java/com/aldeerdev/sistemabancario/domain/model/entity/Cliente.java
@@ -0,0 +1,12 @@
+package com.aldeerdev.sistemabancario.domain.model.entity;
+
+import java.util.List;
+
+public class Cliente {
+	
+	private String nombre;
+	private String documento;
+	private List<CuentaBancaria> cuentas;
+	
+	
+}
