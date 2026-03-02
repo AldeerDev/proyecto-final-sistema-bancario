f046701d4cc47078c08cd18b066ac4f5a6696737
AldeerDev <aldeerdev@gmail.com>
2026-03-02 20:43:29 -0300

    feat: agrega metodos getters

--- src/main/java/com/aldeerdev/sistemabancario/domain/model/entity/Cliente.java
+++ src/main/java/com/aldeerdev/sistemabancario/domain/model/entity/Cliente.java
@@ -5,19 +5,31 @@ import java.util.List;
 import java.util.Objects;
 
 public class Cliente {
-	
+
 	private String nombre;
 	private String documento;
 	private List<CuentaBancaria> cuentas;
-	
+
 	public Cliente(String nombre, String documento) {
 		// validar argumentos nulos
 		Objects.requireNonNull(nombre, "nombre no puede ser nulo.");
 		Objects.requireNonNull(documento, "documento no puede ser nulo.");
-		
+
 		this.nombre = nombre;
 		this.documento = documento;
 		this.cuentas = new ArrayList<>();
 	}
-	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public List<CuentaBancaria> getCuentas() {
+		return cuentas;
+	}
+
 }
