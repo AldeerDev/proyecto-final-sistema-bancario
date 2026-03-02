a072ee2b8a833277933c5c0011ddac8ebc75b333
AldeerDev <aldeerdev@gmail.com>
2026-03-02 20:33:00 -0300

    feat: agrega constructor a la entidad cliente con validaciones

--- src/main/java/com/aldeerdev/sistemabancario/domain/model/entity/Cliente.java
+++ src/main/java/com/aldeerdev/sistemabancario/domain/model/entity/Cliente.java
@@ -1,6 +1,8 @@
 package com.aldeerdev.sistemabancario.domain.model.entity;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Cliente {
 	
@@ -8,5 +10,14 @@ public class Cliente {
 	private String documento;
 	private List<CuentaBancaria> cuentas;
 	
+	public Cliente(String nombre, String documento) {
+		// validar argumentos nulos
+		Objects.requireNonNull(nombre, "nombre no puede ser nulo.");
+		Objects.requireNonNull(documento, "documento no puede ser nulo.");
+		
+		this.nombre = nombre;
+		this.documento = documento;
+		this.cuentas = new ArrayList<>();
+	}
 	
 }
